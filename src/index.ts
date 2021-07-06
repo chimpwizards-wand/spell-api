e33f19ebaa24bcabfadf7548d3ae7ce5aae1bd30
ndruwizz <ndru@chimpwizard.com>
2021-07-06 07:59:34 -0500

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-export * as API from './commands/API';
-export * as Registry from './commands/Registry';
-export * as Define from './commands/registry/Define';
-export * as Add from './commands/registry/Add';
+export * as p0 from './commands/API';
+export * as p2 from './commands/Registry';
+export * as p3 from './commands/registry/Define';
+export * as p4 from './commands/registry/Add';
+
+//Individual classes
+export { Define }  from './commands/registry/Define';
