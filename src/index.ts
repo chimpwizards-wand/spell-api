b207fc8811a4b2d46db48393a5e9c33dacce02fc
ndruwizz <ndru@chimpwizard.com>
2021-07-06 12:09:18 -0500

    Add api handelr

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export * as p0 from './commands/API';
+export * as p1 from './commands/api/Handler';
 export * as p2 from './commands/Registry';
 export * as p3 from './commands/registry/Define';
 export * as p4 from './commands/registry/Add';
 
 //Individual classes
 export { Define }  from './commands/registry/Define';
+export { Handler }  from './commands/api/Handler';
