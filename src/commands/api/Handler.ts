7be9d6ec24ac78895caac664c2e9b4a60452147f
ndruwizz <ndru@chimpwizard.com>
2021-07-06 12:11:27 -0500

    Add api handelr

---
+++ src/commands/api/Handler.ts
@@ -0,0 +1,13 @@
+import Debug from 'debug';
+import { Command } from  '@chimpwizards/wand'
+const chalk = require('chalk');
+const debug = Debug("w:cli:api:handler");
+
+export class Handler  { 
+
+    execute(yargs: any): void {
+        debug(`Do Nothing`)
+        
+    } 
+
+}
